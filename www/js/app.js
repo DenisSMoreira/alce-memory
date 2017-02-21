0a94d1788aa5acbee6c9ce205fe85e46e7637d4d
DenisSMoreira <denis.soares.moreira@gmail.com>
2017-02-21 14:50:31 -0300

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,60 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var memoryGameApp = angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+memoryGameApp.factory('game', function() {
+  var tileNames = ['8-ball', 'kronos', 'baked-potato', 'dinosaur', 'rocket', 'skinny-unicorn',
+    'that-guy', 'zeppelin'];
+
+  return new Game(tileNames);
+});
+
+
+memoryGameApp.controller('GameCtrl', function GameCtrl($scope, game) {
+  $scope.game = game;
+});
+
+
+//usages:
+//- in the repeater as: <mg-card tile="tile"></mg-card>
+//- card currently being matched as: <mg-card tile="game.firstPick"></mg-card>
+
+memoryGameApp.directive('mgCard', function() {
+  return {
+    restrict: 'E',
+    // instead of inlining the template string here, one could use templateUrl: 'mg-card.html'
+    // and then either create a mg-card.html file with the content or add
+    // <script type="text/ng-template" id="mg-card.html">.. template here.. </script> element to
+    // index.html
+    template: '<div class="container">' +
+                '<div class="card" ng-class="{flipped: tile.flipped}">' +
+                  '<img class="front" ng-src="img/back.png">' +
+                  '<img class="back" ng-src="img/{{tile.title}}.png">' +
+                '</div>' +
+              '</div>',
+    scope: {
+      tile: '='
+    }
+  };
+});
